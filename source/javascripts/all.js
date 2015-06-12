d7a26533826c1591892a532124d2e64f0e6493d6
Byess, James <James.Byess@washpost.com>
2015-06-11 23:45:11 -0400

    what am i doing

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,118 +1,146 @@
-(function() {
-
 /* jshint -W058 */
 document.addEventListener('DOMContentLoaded', function(event) {
 
-	function init() {
-		console.log('init fired');
+function drawBoard() {
+	var board = document.getElementById('boardcontainer');
 
-		//define board drawing functions
-		function drawBoard() {
+	//make 49 squares. board is 7x7, duh.
+	for(var i = 1; i <= 49; i++) {
+		var sq = document.createElement('div');
+		sq.setAttribute('id','sq'+i);
+		sq.setAttribute('class','square');
+		board.appendChild(sq);
+	}
+}
 
-			var board = document.getElementById('boardcontainer');
-			
-			for(var i = 1; i <= 49; i++) {
-				var sq = document.createElement('div');
-				sq.setAttribute('id','sq'+i);
-				sq.setAttribute('class','square');
-				board.appendChild(sq);
-			}
 
+Data = {
+	level : 1,
+	score : {},
+	stats : {}
+};
+
+gameUtils = {	
+	events : {
+		click : function() {
+			console.log(this);
 		}
-
-		function setupObjects(){
-			Data = {
-				level : 1,
-				score : {},
-				stats : {}
-			};
-			Game = {
-				fps : '40',
-				events : {
-					click : function() {
-						console.log(this);
-					}
-				},
-				draw : {
-					pickRandomColor : function(obj, num) {
-						//obj = colors object, num = number of colors to pick from
-						var colors = Object.keys(obj);
-						return obj[colors[num * Math.random() << 0]];
-					},
-					fillSquares : function(num) {
-						//num defines how many colored squares to get
-						for(var i=1; i <= num; i++) {
-							var randomId = Math.floor(Math.random() * 49 +1);
-							var squareToFill = document.getElementById('sq'+randomId);
-							squareToFill.style.backgroundColor = Game.draw.pickRandomColor(Game.colors, 3);
-						}
-					}
-				},
-				colors : {
-					'blue' : '#2882ff',
-					'red' : '#ff141d',
-					'green' : '#00f40d',
-					'orange' : '#ff9300',
-					'purple' : '#ff00bd',
-					'cyan' : '#14fcff'
-				}
-			};
-			timestamp = function() {
-				return window.performance && window.performance.now ? window.performance.now() : (new Date()).getTime();
-			};
-		}
-		function setupListeners() {
-			var s = document.getElementsByClassName('square');
-			for(var i = 0; i < s.length; i++){
-				s[i].addEventListener('click', Game.events.click, false);
+	},
+	draw : {
+		pickRandomColor : function(obj, num) {
+			//obj = colors object, num = number of colors to pick from
+			var colors = Object.keys(obj);
+			return obj[colors[num * Math.random() << 0]];
+		},
+		fillSquares : function(num) {
+			//num defines how many colored squares to get
+			for(var i=1; i <= num; i++) {
+				var randomId = Math.floor(Math.random() * 49 +1);
+				var squareToFill = document.getElementById('sq'+randomId);
+				squareToFill.style.backgroundColor = Game.draw.pickRandomColor(Game.colors, 3);
 			}
 		}
+	},
+	colors : {
+		'blue' : '#2882ff',
+		'red' : '#ff141d',
+		'green' : '#00f40d',
+		'orange' : '#ff9300',
+		'purple' : '#ff00bd',
+		'cyan' : '#14fcff'
+	}
+};
+timestamp = function() {
+	return window.performance && window.performance.now ? window.performance.now() : (new Date()).getTime();
+};
 
+function setupListeners() {
+	// var s = document.getElementsByClassName('square');
+	// for(var i = 0; i < s.length; i++){
+	// 	s[i].addEventListener('click', Game.events.click, false);
+	// }
+}
 
-		drawBoard();
-		setupObjects();
-		setupListeners();
+function Game() {
+	//main game is here
+	console.log('game is cool');
+
+	var fps = 40,
+	tick = 0;
+
+	//listeners for squares
+	var squareListeners = document.getElementsByClassName('square');
+	for(var i = 0; i < squareListeners.length; i++){
+		squareListeners[i].addEventListener('click', Game.events.click, false);
 	}
 
-	//init stuff
-	init();
+	window.requestAnimationFrame =
+	    window.requestAnimationFrame ||
+	    window.mozRequestAnimationFrame ||
+	    window.webkitRequestAnimationFrame ||
+	    window.oRequestAnimationFrame;
 
-	
-	//control flow here
-	Game.run = (function() {
-		var loops = 0, skipTicks = 1000 / Game.fps,
-		maxFrameSkip = 10,
-		nextGameTick = timestamp();
+	window.requestAnimationFrame(this.draw.bind(this));
+}
 
-		return function() {
-			loops = 0;
+Game.prototype.draw = function() {
+	tick++;
+	gameUtils.draw.fillSquares(3);
 
-			while ((new Date).getTime() > nextGameTick && loops < maxFrameSkip ) {
-				//Game.update();
-				nextGameTick += skipTicks;
-				loops++;		
-			}
+};
+Game.prototype.update = function() {
 
-			Game.draw.fillSquares(3);
+};
+
+Game.prototype.init = function() {
+	console.log('init fired');
+
+	//define board drawing functions
+
+
+	drawBoard();
+	setupObjects();
+	setupListeners();
+};
+
+//init stuff
+
+
+var game = new Game();
+
+
+
+//control flow here
+// Game.run = (function() {
+// 	var loops = 0, skipTicks = 1000 / Game.fps,
+// 	maxFrameSkip = 10,
+// 	nextGameTick = timestamp();
+
+// 	return function() {
+// 		loops = 0;
+
+// 		while ((new Date).getTime() > nextGameTick && loops < maxFrameSkip ) {
+// 			Game.update();
+// 			nextGameTick += skipTicks;
+// 			loops++;		
+// 		}
+
 		
-
-		
-	};
-	window.clearInterval(Game._intervalId);
-
-	})();
-
 	
 
-
-	//actual game loop here
-	Game._intervalId = window.setInterval(Game.run, 1000 / Game.fps);
-
 	
-	
+// // };
+// window.clearInterval(Game._intervalId);
+
+// })();
+
+
+
+
+// //actual game loop here
+// Game._intervalId = window.setInterval(Game.run, 1000 / Game.fps);
+
 
 
 
 });
-
-})();
