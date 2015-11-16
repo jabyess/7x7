e08bef4eaf049feaccc6a7925c402ff2daed5274
jabyess <James.Byess@washpost.com>
2015-11-16 11:10:09 -0500

    backup before os rollback

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -12,6 +12,7 @@ function drawBoard() {
 		var sq = document.createElement('div');
 		sq.setAttribute('id','sq'+i);
 		sq.setAttribute('class','square');
+		sq.dataset.active = false;
 		board.appendChild(sq);
 	}
 }
@@ -22,6 +23,9 @@ gameData = {
 	stats : {
 		clicks: 0,
 		moves: 0
+	},
+	squares : {
+
 	}
 };
 
@@ -54,6 +58,81 @@ gameUtils = {
 			}
 		}
 	},
+	update: {
+		checkActiveSquares: function(activeObj) {
+			//returns true if more than one square is higlighted
+			//takes gameData.squares.active as preferred object.
+			var size = 0, s;
+			for(s in activeObj) {
+				if(activeObj.hasOwnProperty(s)){
+					size++;
+				}
+			}
+			if(size > 1) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		},
+		highlightFunction: function(hasColor, isActive) {
+			// console.log(this);
+			function highlightSquare() {
+				console.log(this);
+				this.style.transform = 'scale(1.1)';
+				this.dataset.active = true;
+				// squareIsActive = true;	
+				console.log('made active');
+			}
+			function unhighlightSquare() {
+				this.style.transform = 'scale(1.0)';
+				this.dataset.active = false;
+				console.log('already active');
+			}
+			function makeAllSquaresInactive() {
+
+			}
+			if(hasColor && isActive === 'true') {
+				unhighlightSquare.bind(this);
+			}
+			else if(gameUtils.update.checkActiveSquares(gameData.squares.active) === true) {
+				var activeSquares = document.querySelector('[data-active="true"]');
+				activeSquares.dataset.active = 'false';
+				unhighlightSquare(this);
+				console.log(activeSquares);
+				console.log('another square is active');
+
+			}
+			else if(hasColor && isActive === 'false') {
+				highlightSquare.bind(this);
+				
+			}
+			else {
+				console.log('not active');
+			}
+		}
+
+	},
+	init: {
+		squareObjGenerator : function() {
+			var defaults = {
+				'color' : '#fff',
+				'selected' : false
+			};
+			for(var j = 1; j <= 49; j++) {
+				// gameData.squares['sq'+j] = new gameData.squares(defaults);
+				var sqName = 'sq'+j;
+				gameData.squares[sqName] = defaults;
+			}
+		},
+	},
+	
+	squareListeners : function() {
+		//listeners for squares
+		//get this working with bind duh
+
+
+	},
 	colors : {
 		'blue' : '#2882ff',
 		'red' : '#ff141d',
@@ -70,11 +149,7 @@ function Game() {
 
 	this.init();
 
-	//listeners for squares
-	var squareListeners = document.getElementsByClassName('square');
-	for(var i = 0; i < squareListeners.length; i++){
-		squareListeners[i].addEventListener('click', this.onclick, false);
-	}
+	
 
 	window.requestAnimationFrame =
 	    window.requestAnimationFrame ||
@@ -92,7 +167,7 @@ Game.prototype.draw = function() {
 		tick = 0;
 	}
 
-	this.drawNumbers();
+	
 	this.update();
 
 
@@ -100,41 +175,62 @@ Game.prototype.draw = function() {
 };
 
 Game.prototype.update = function() {
+	this.updateNumbers();
+
+	this.statusUpdateLoop();
+
 
 
 };
 
-Game.prototype.onclick = function() {
+Game.prototype.onClick = function() {
 	gameData.stats.clicks++;
-	var squareIsActive = squareIsActive || false;
+	// var squareIsActive = squareIsActive || false;
+	var isActive = this.dataset.active;
+	console.log(gameData);
 
-	function highlightSquare(hasColor, isActive) {
-		if(hasColor && !isActive) {
-			this.style.transform = 'scale(1.1)';
-			squareIsActive = true;	
-			console.log('made active');
-		}	
-		console.log('else');
-	}
+
 	//move square
 	// if(!this.style.backgroundColor && !squareIsActive) {
 	// 	squareIsActive = false;
 	// }
 
-	var wat = highlightSquare.bind(this, this.style.backgroundColor, squareIsActive);
+	var wat = gameUtils.update.highlightFunction.bind(this, this.style.backgroundColor, isActive);
 	wat();
 
 };
 
-Game.prototype.drawNumbers = function() {
+Game.prototype.updateNumbers = function() {
 	var stats = stats || document.getElementById('clicks');
 	stats.innerHTML = gameData.stats.clicks;
 
+};
+Game.prototype.statusUpdateLoop = function() {
+	var squareCollection = document.getElementsByClassName('square');
+	gameData.squares.active = {};
+	for(var i = 0; i < squareCollection.length; i++ ) {
+		if(squareCollection[i].dataset.active === 'true') {
+			var tmp = {};
+			tmp[squareCollection[i].id] = true;
+			gameData.squares.active += tmp;
+		}
+
+	}
 };
 Game.prototype.init = function() {
 	console.log('init fired');
 
 	drawBoard();
+
+	// gameUtils.squareListeners();
+	var squareListeners = document.getElementsByClassName('square');
+	for(var i = 0; i < squareListeners.length; i++){
+		squareListeners[i].addEventListener('click', this.onClick, false);
+	}
+
+	gameUtils.init.squareObjGenerator();
+	console.log(gameData);
+
 	gameUtils.draw.fillEmptySquares(3);
 };
 
